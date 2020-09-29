7e062ae4157927a1911c4d20197039666a74738f
Ruben <ruben.thomas.nj@gmail.com>
2020-09-29 10:50:43 -0400

    Updates

---
+++ precache-manifest.f419dc442744ad16ed247c613af5e0ae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05bd0c8aae7146568815858c0c132172",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "28cf2ce4628f0b307345",
+    "url": "/my-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "666d17ed9f45f4bb6aaf",
+    "url": "/my-app/static/js/2.1a054728.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-app/static/js/2.1a054728.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "28cf2ce4628f0b307345",
+    "url": "/my-app/static/js/main.48d4ef61.chunk.js"
+  },
+  {
+    "revision": "922aae306dac65436efd",
+    "url": "/my-app/static/js/runtime-main.2e18c7ca.js"
+  }
+]);
