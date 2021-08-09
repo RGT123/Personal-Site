137a9b7cb734476d1dcf36f6cff3623c2372f735
Ruben <ruben.thomas.nj@gmail.com>
2021-08-09 17:53:19 -0400

    Background update

--- src/HomeScreen.js
+++ src/HomeScreen.js
@@ -1,24 +1,27 @@
 import React from 'react';
-
+// 10D2D2
 import './App.css';
 import 'mdbreact/dist/css/mdb.css';
-
+import "./index.css"
 import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import face from "../src/assets/face_round.png"
-import bg from "../src/assets/bg_frame.png"
+import bg from "../src/assets/bg_frame_july2021.png"
+import logo from "../src/assets/logo.png"
+import ReactFloaterJs from 'react-floaterjs'
 var styles = {
    backgroundImage: "url(" + bg + ")",
    height: "100%",
    minHeight: "100vh",
-   width: "100vw",
+   width: "100%",
+   minWidth: "100vw",
    backgroundPosition: "center",
    backgroundRepeat: "no-repeat",
    backgroundSize: "cover",
-   backgroundColor: "#037AFF",
+   backgroundColor: "#000000",
    backgroundAttachment: "fixed",
  }
 class HomeScreen extends React.Component {
@@ -29,21 +32,12 @@ class HomeScreen extends React.Component {
       return (
          <Container fluid className="p-0" style={styles}>
            
-           <Row  className="align-items-center">
-             
-            <Col>
-              {/* <img src={face} style={{maxHeight: "60vh",maxWidth: "60vw", padding: 25}}/> */}
-              
            
-            
-              </Col>
-          
-           </Row>
-           <Row >
+           <Row style={{maxWidth: "100vw"}}>
              <Col className="d-flex justify-content-center" >
                <div style={{padding: 10}}>
                 
-                <h4 className="textTop" style={{paddingTop: "50px",fontSize: "30px", fontStyle: "italic"}}>"True humility is not thinking less of yourself; it is thinking of yourself less."</h4>
+                <h4 className="textTop" style={{paddingTop: "50px",fontSize: "30px", fontStyle: "italic"}}>"True humility is not <b style={{fontWeight: "bolder", color: "#ffffff"}}>thinking less</b> of yourself; it is thinking of yourself <b style={{fontWeight: "bolder", color: "#ffffff"}}>less</b>."</h4>
                
                 <br/>
                 <h5 className="textTop" style={{paddingLeft: "50px", fontSize: "30px",fontStyle: "italic"}}> -C.S. Lewis</h5>
@@ -52,9 +46,13 @@ class HomeScreen extends React.Component {
              </Col>
            </Row>
            
-            <Row>
-            <h1 className="textTop" style={{ padding: "200px", fontSize: "100px"}}>Hi, I'm Ruben!</h1>
             
+            <Row  >
+              <Col style ={{position:"absolute", bottom:10}} className="d-flex justify-content-center">
+              <ReactFloaterJs>
+                  <h4 style={{color: "white"}}>Scroll Down!</h4>
+                  </ReactFloaterJs>
+                </Col>
             </Row>
             
          </Container>
--- src/Skills.js
+++ src/Skills.js
@@ -1,13 +1,14 @@
 import React from 'react';
 
 import './App.css';
+import './Skills.css'
 import Header from './Header';
 import Button from 'react-bootstrap/Button';
 import {Container, Card} from 'react-bootstrap';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import bg from "../src/assets/bg1.png";
+import bg from "../src/assets/bg1Art.png";
 import rutgers from "../src/assets/rutgers_logo.jpg";
 import gm from "../src/assets/gm_small.png";
 import code from "../src/assets/code_logo.png";
@@ -31,22 +32,21 @@ class Skills extends React.Component {
    render() {
       return (
          <Container fluid style={styles}>
-            <Row style={{maxWidth: "60vw", paddingTop: 100, paddingLeft: 100}}>
-               <Col style={{ padding: 50, maxWidth: "25vw"}}>
-                  <Card style={{padding: 10}}>
+            <Row className="topRow">
+               <Col sm="12" md="12" lg="6" style={{padding: 40}}>
+                  <Card style={{padding: 30, minWidth: "250px"}}>
                      <Card.Img src={rutgers}></Card.Img>
                      <Card.Title>Education</Card.Title>
                      <Card.Body>
                         <ul className="list-group list-group-flush">
-                           <li className="list-group-item">Rutgers University - Computer Science, BS 2021</li>
+                           <li className="list-group-item">Rutgers University - Computer Science BS, Economics Minor January 2021</li>
                            <li className="list-group-item">East Brunswick Highschool - 2017</li>
                         </ul></Card.Body>
                   </Card>
-                  
-                  
-               </Col>
-               <Col style={{ padding: 50, maxWidth: "25vw"}}>
-                  <Card style={{padding: 10}}>
+                  </Col>
+               <Col sm="12" md="12" lg="6" style={{padding: 40}}>  
+               
+                  <Card style={{padding: 30, minWidth: "250px"}}>
                      <Card.Img src = {gm} ></Card.Img>
                      <Card.Title>Experience</Card.Title>
                      <Card.Body>
@@ -58,10 +58,9 @@ class Skills extends React.Component {
                   
                   
                </Col>
-            </Row>
-            <Row style={{maxWidth: "60vw", paddingLeft: 100}}>
-            <Col style={{ padding: 50, maxWidth: "25vw"}}>
-                  <Card style={{padding: 30}}>
+            
+            <Col sm="12" md="12" lg="6" style={{padding: 40}}>
+                  <Card style={{padding: 30, minWidth: "250px"}}>
                      <Card.Img src = {code} ></Card.Img>
                      <Card.Title>Skills</Card.Title>
                      <Card.Body>
@@ -73,8 +72,8 @@ class Skills extends React.Component {
                
                   
                </Col>
-               <Col style={{ padding: 50, maxWidth: "25vw"}}>
-                  <Card style={{padding: 30}}>
+               <Col sm="12" md="12" lg="6" style={{padding: 40}}>
+                  <Card style={{padding: 30, minWidth: "250px"}}>
                      <Card.Img src={social}></Card.Img>
                      <Card.Title>Follow Me</Card.Title>
                      <Card.Body>
